772d93bf911c3c3fb7f3247500808d939a5018e6
thelrner <peteryhua@gmail.com>
2016-03-01 07:59:14 -0500

    Fixes build_weights(), list_comprehensifies regularization calc. Adds 2 lines of Readme.

---
+++ README.md
@@ -0,0 +1,5 @@
+CONGRATS YOU'VE FOUND ME!
+
+
+
+Vectorized implementations have been implemented in Octave, and will be ported over to NumPy eventually. In the meantime, please enjoy some for loops and list comprehensions.
--- neural_net.py
+++ neural_net.py
@@ -26,9 +26,6 @@ class NeuralNet(object):
     def train():
         pass
 
-    def forward_prop():
-        pass
-
     def activate():
         pass
 
@@ -48,8 +45,10 @@ class NeuralNet(object):
         self.set_defaults()
 
     def set_defaults(self):         # defaults that can be overridden at runtime
-        self.reg_term = 1           #PH: reasonable?
+        self.reg_rate = 1           #PH: reasonable?
         self.epsilon = 1       #PH: look at suggested here, or revisit
+
+        self.data_size = len(self.training_data)        #PH: confusing naming?
 
         self.input_size = 2
         self.input_size += 1        # add bias
@@ -63,7 +62,6 @@ class NeuralNet(object):
 
         self.build_nodes()
         self.build_weights()
-        set_trace()
         self.weights = [
                         [[30, -20, -20], [-10, 20, 20]],       # [NOT AND], [OR]
                         [[-30, 20, 20]]                         # [AND]
@@ -80,7 +78,7 @@ class NeuralNet(object):
         # figure out right # hidden layer...
         # self.hidden_layer = max(2, self.suggested_hidden_layers())
 
-    def parse_and_set(inputs):
+    def parse_and_set(self, inputs):
         # set input_size, output_size, output_layer_i, sizes?
         pass
 
@@ -94,11 +92,18 @@ class NeuralNet(object):
 
     def build_weights(self):
         # For each layer, as many rows as there are nodes in the next layer. As many elements per row as there are nodes in current layer.
-        self.weights = [ [ [ #uniform(-self.epsilon, self.epsilon)
-                             1
-                             for node in xrange(curr_size) ]
-                           for node in xrange(self.sizes[i + 1]) ]
-                         for i, curr_size in enumerate(self.sizes[:-1]) ]
+        self.weights = [ [ [ uniform(-self.epsilon, self.epsilon)
+                             for curr_node_i in xrange(curr_size) ]
+                           #PH: maybe extrap out the size index picker?
+                           # should only go to next sizes - 1 in next_node_i, because we don't use the current nodes to calculate BIAS unit of next layer, unless next layer is output!
+                           for next_node_i in xrange(self.sizes[i + 1] - 1) ]
+                         for i, curr_size in enumerate(self.sizes[:-2]) ]
+
+        # For final output weights, we don't skip the bias unit in the next_node_i
+        output_weights = [ [ uniform(-self.epsilon, self.epsilon)
+                             for pre_output_node_i in xrange(self.sizes[-2]) ]
+                           for output_node_i in xrange(self.sizes[-1]) ]
+        self.weights.append(output_weights)
 
     def build_errors_matrix(self):
         self.errors = utils.dupe_with_randos(self.weights)
@@ -112,18 +117,19 @@ class NeuralNet(object):
     def train(self):
         training_iters = izip(self.input_iter(), self.output_iter())
         for input_row, output_val in self.training_data:
-            pass        #PH:*** write this out! First, forward_prop, then backward...
+            self.run(input_row)
+            self.back_prop()
 
     def run(self, input_row):
         NeuralNet.add_bias(input_row)
         self.nodes[0] = input_row
         self.output_layer_i = len(self.nodes) - 1
 
-        self.forward_prop()
+        self.feed_forward()
         return self.nodes[self.output_layer_i]
 
     #PH: extrapolate out some of these methods so you don't gotta worry about +1, -1, etc.
-    def forward_prop(self):
+    def feed_forward(self):
         # skip the input layer
         for layer_i in xrange(0, self.output_layer_i):
             curr_layer_nodes = self.nodes[layer_i]
@@ -151,6 +157,9 @@ class NeuralNet(object):
             self.activate(layer_i + 1)
             print self.nodes
 
+    def back_prop(self):
+        pass
+
     def activate(self, layer_i):
         layer_nodes = self.nodes[layer_i]
         for i, node_val in enumerate(layer_nodes):
@@ -160,12 +169,6 @@ class NeuralNet(object):
                 continue
             self.nodes[layer_i][i] = utils.sigmoid(node_val)
 
-    def output_iter(self):
-        return (item['output'] for item in self.training_data)
-
-    def input_iter(self):
-        return (item['input'] for item in self.training_data)
-
     # I assume you use this after you run all the inputs and have the predicted outputs for every one?
     # Really, you need to do this one as you're looping through and calculating the output. In a neural net, the network itself IS the hypothesis Fn... remember that calc_error is the only place we ever call the hypothesis Fn from? Well, you're gonna need to do that now with the network... probably.
     # REM the nodes don't have to be refreshed in between runs
@@ -174,15 +177,15 @@ class NeuralNet(object):
         reg_term = self.calc_error_regularization()
 
     def calc_error_regularization(self):
-        reg = 0
+        all_weights_iter = (
+            curr_layer_weight ** 2 if i else 0      # implicit 0
+            for layer in self.weights
+            for next_layer_row in layer
+            # don't want to add bias of curr_layer into reg term
+            for i, curr_layer_weight in enumerate(next_layer_row)
+        )
 
-        for layer_weights in self.weights:
-            for curr_layer_weights in layer_weights:
-                for i, weight in enumerate(curr_layer_weights):
-                    if i != 0:
-                        reg += (weight ** 2)
-        return reg * self.reg_term / (2 * len(self.training_data))
-
+        return sum(all_weights_iter) * self.reg_rate / (2 * self.data_size)
 
 
 net = NeuralNet([[1, 0, 0], [0, 1, 1]])
@@ -202,7 +205,35 @@ Ie. in XOR...
 NOT AND -->
 '''
 
+# PH: Non-list-comp setting own weights
+# self.other_weights = []
+# for i, curr_size in enumerate(self.sizes[:-1]):
+#     self.other_weights.append([])
+#     level_2 = self.other_weights[i]
+#
+#     # -1 bc don't use curr nodes to calculate bias unit of next nodes
+#     for next_node_i in xrange(self.sizes[i + 1] - 1):
+#         level_2.append([])
+#         level_3 = level_2[next_node_i]
+#
+#         for curr_node_i in xrange(curr_size):
+#             level_3.append(1)
 
+# PH: Non-list-comp regularization
+# def calc_error_regularization(self):
+#     reg = 0
+#
+#     for layer_weights in self.weights:
+#         for curr_layer_weights in layer_weights:
+#             for i, weight in enumerate(curr_layer_weights):
+#                 if i != 0:
+#                     reg += (weight ** 2)
+#     return reg * self.reg_rate / (2 * self.data_size)
+
+# For weights node sizes ....
+# self.sizes = [3, 4, 5, 2]
+# NOTE: the first weights will be (4 - 1) x 3, the second (5 - 1) x 4, etc.
+# ^ This will be the size of the matrices
 
 from random import randrange
 from functools import partial
