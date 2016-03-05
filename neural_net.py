e1d74d582e6d4cc72ed88c7a5033df5a75af0bc4
thelrner <peteryhua@gmail.com>
2016-03-04 23:39:17 -0500

    Transfers code from test file to main file. Deletes neural_net_test.py.

--- neural_net.py
+++ neural_net.py
@@ -6,7 +6,20 @@ from random import uniform
 import utils
 
 '''
-Pruning algo -- disregard nodes that receive low weights
+NOTES ON XOR BATCH ANN GRADIENT DESCENT
+1) Low epsilon (0.15) tends all weights toward 0.5
+2) Having more nodes in hidden layer increases chance of accurate training. Bigger brainz
+
+- Follow rest of notes (set logistic and linear regression to take [0], [1] for outputs)
+- Implement error threshold for neural nets, and "convergence" theta-change threshold for logistic and linear regression
+- Set intelligent defaults for node_size, # hidden layers, etc.
+- Use intelligent epsilons!
+
+- Finish readme -- each section needs its own. ANN first.
+- Humanize all variable names. No thetas & alphas.
+
+* Hidden_sizes needs to be user inputted, along with other options.
+
 '''
 
 #http://stats.stackexchange.com/questions/181/how-to-choose-the-number-of-hidden-layers-and-nodes-in-a-feedforward-neural-netw
@@ -44,8 +57,10 @@ class NeuralNet(object):
 
     def set_defaults(self):         # defaults that can be overridden at runtime
         self.add_training_bias()
-        self.reg_rate = 0.0
-        self.epsilon = 1       #PH: look at suggested here, or revisit
+        self.reg_rate = 0.0000
+        self.epsilon = 2       #PH: look at suggested here, or revisit
+
+        self.momentum = 1.1
 
         self.data_size = len(self.training_data)        #PH: confusing naming?
 
@@ -53,36 +68,29 @@ class NeuralNet(object):
         self.input_size += 1        # add bias
         self.output_size = 1        # read from training_data
 
-        #PH:*** have defaults, allow USER to SET hidden_sizes
-        self.hidden_sizes = [2]
-        # add biases
-        self.hidden_sizes = [size + 1 for size in self.hidden_sizes]
+        #PH:*** unbiased_hiddens = `user_submission` or self.recommended()
+        unbiased_hiddens = self.recommended_hidden_sizes()
+        self.hidden_sizes = [size + 1 for size in unbiased_hiddens]
         self.sizes = [self.input_size] + self.hidden_sizes + [self.output_size]
 
         self.build_nodes()
-        print self.nodes
         self.weights = self.build_weights()
+
         self.gradients = utils.dupe_with_infs(self.weights)
 
         # structure same as nodes, without bias
         self.deltas = utils.dupe_with_infs(self.nodes)
 
-        #PH:*** delete this...
-        # self.weights = [[[0.36483230176638926, -0.10354547213358176, -0.7980308463608701], [0.9237207276836605, -0.21646020063063934, -0.23030769700312906]], [[0.6545288228362074, 0.9406124797474309, 0.08076250834392552]]]
-
-        # On choosing epsilons for random initialization...
-        # One effective strategy for choosing epsilon is to base it on the number of units in the network. A good choice of is... LOOK UP
-
         #PH:*** redo the threshold here. you're trying to CONVERGE. not REACH ZERO ERROR
-        self.learn_rate = 0.1               # learn rate   PH:*** rename?
-        self.max_iters = 100                 # change
-
-        # figure out right # hidden layer...
-        # self.hidden_layer = max(2, self.suggested_hidden_layers())
+        self.learn_rate = 0.05               # learn rate   PH:*** rename?
+        self.max_iters = 10000                 # PH: change
 
     def add_training_bias(self):
         for input_row in self.input_iter:
             NeuralNet.add_bias(input_row)
+
+    def recommended_hidden_sizes(self):
+        return [ max(4, 1 + abs(self.input_size - self.output_size) / 2) ]
 
     def parse_and_set(self, inputs):
         # set input_size, output_size, output_layer_i, sizes?
@@ -112,6 +120,20 @@ class NeuralNet(object):
         weights.append(output_weights)
         return weights
 
+    def build_weights_suggested(self):
+        self.weights = []
+        for i, curr_size in enumerate(self.sizes[:-2]):
+            self.weights.append([])
+            layer_weights = self.weights[i]
+
+            for next_node_i in xrange(self.sizes[i + 1] - 1):
+                layer_weights.append([])
+                next_i_weights = layer_weights[next_node_i]
+
+                for curr_node_i in xrange(curr_size):
+                    epsilon = 0.15
+                    next_i_weights.append()
+
     def train(self):
         iters = 0
         #PH:*** add more logic here
@@ -122,17 +144,17 @@ class NeuralNet(object):
                 self.reset_deltas()
 
                 self.feed_forward(input_row)
-                self.back_prop(output_row)      #PH:*** you'll need to accum errors here somehow. can't just back_prop line by line...
+                self.back_prop(output_row)
 
             self.postprocess_gradients()
             self.set_new_weights()          #PH:*** calc here, and test
             iters += 1
-            self.log_things()
+            # self.log_things()
 
     def log_things(self):
         new_error = self.calc_error()
         print 'Nodes: %s' % self.nodes
-        print 'New error: %s' % new_error
+        # print 'New error: %s' % new_error
         print 'Weights: %s' % self.weights
         print 'Gradients: %s' % self.gradients
         print 'Deltas: %s' % self.deltas
@@ -144,7 +166,6 @@ class NeuralNet(object):
 
     def reset_deltas(self):
         #PH: fix this. duping don't make sense. how about fill_with_zeros instead?
-        #PH: is it RIGHT to always reset deltas on EVERY ROW? think so...
         self.deltas = utils.dupe_with_zeros(self.deltas)
 
     def run(self, user_given_input_row):
@@ -183,6 +204,7 @@ class NeuralNet(object):
             self.activate(layer_i + 1)
         # print 'After forward feed: %s' % self.nodes
 
+    #PH: move closer to train() function in positioning O_O
     def back_prop(self, output_row):
         self.set_deltas(output_row)
         self.accumulate_gradient(output_row)
@@ -250,7 +272,7 @@ class NeuralNet(object):
             for i, next_i_gradients in enumerate(layer_gradients):
                 for j, gradient in enumerate(next_i_gradients):
                     gradient_val = gradient / self.data_size
-                    if j:       # j != 0
+                    if j != 0:
                         gradient_val += self.reg_rate * self.weights[l][i][j]
                     next_i_gradients[j] = gradient_val
 
@@ -261,7 +283,8 @@ class NeuralNet(object):
             for i, next_i_weights in enumerate(layer_weights):
                 for j, weight in enumerate(next_i_weights):
                     gradient_val = self.gradients[l][i][j]
-                    next_i_weights[j] = weight - self.learn_rate * gradient_val
+                    change = self.momentum * self.learn_rate * gradient_val
+                    next_i_weights[j] = weight - change
 
     def activate(self, layer_i):
         layer_nodes = self.nodes[layer_i]
@@ -303,102 +326,25 @@ class NeuralNet(object):
 
         return sum(all_weights_iter) * self.reg_rate / (2 * self.data_size)
 
+# PH:*** Conclusion from tests -- our normal training iterations perform the EXACT same function as a no-hidden-layer logistic regression. Frickin hell bro. Are we not running separate gradient descents on each intermediate delta?
+
+# PH:*** REM: Try to go through 1 round of iteration with 1 or 2 row(s) of training data -- match each number!
+
+# PH:*** BUILD YOUR THETAS MANUALLY, THEN UNIT TEST VERSUS OCTAVE
+#   ie. run your delta code -- does it make sense? is backprop giving consistent results?
+
+# PH:*** UNIT
 
 net = NeuralNet([
     {'input': [1, 0], 'output': [1]},
     {'input': [0, 1], 'output': [1]},
-    {'input': [0, 0], 'output': [0]},
     {'input': [1, 1], 'output': [0]},
+    {'input': [0, 0], 'output': [0]}
 ])
-
-print 'Before training .......'
-print net.run([1, 0])
-print net.run([0, 1])
-print net.run([1, 1])
-print net.run([0, 0])
 
 net.train()
 
-print 'After training .......'
 print net.run([1, 0])
 print net.run([0, 1])
 print net.run([1, 1])
 print net.run([0, 0])
-
-
-
-#PH:*** DELETE all this stuff down here
-
-'''
-Top level of weights is level.
-Second level of weights is the weights for the node of the next layer.
-Third level are the individual weights to multiply the current node by.
-
-Ie. in XOR...
-
-(NOT AND) AND (OR)
-NOT AND -->
-
-        # self.weights = [
-        #                 [[30, -20, -20], [-10, 20, 20]],       # [NOT AND], [OR]
-        #                 [[-30, 20, 20]]                         # [AND]
-        #                ]
-
-'''
-
-# PH: Non-list-comp setting own weights
-# self.other_weights = []
-# for i, curr_size in enumerate(self.sizes[:-1]):
-#     self.other_weights.append([])
-#     level_2 = self.other_weights[i]
-#
-#     # -1 bc don't use curr nodes to calculate bias unit of next nodes
-#     for next_node_i in xrange(self.sizes[i + 1] - 1):
-#         level_2.append([])
-#         level_3 = level_2[next_node_i]
-#
-#         for curr_node_i in xrange(curr_size):
-#             level_3.append(1)
-
-# PH: Non-list-comp regularization
-# def calc_error_regularization(self):
-#     reg = 0
-#
-#     for layer_weights in self.weights:
-#         for curr_layer_weights in layer_weights:
-#             for i, weight in enumerate(curr_layer_weights):
-#                 if i != 0:
-#                     reg += (weight ** 2)
-#     return reg * self.reg_rate / (2 * self.data_size)
-
-# For weights node sizes ....
-# self.sizes = [3, 4, 5, 2]
-# NOTE: the first weights will be (4 - 1) x 3, the second (5 - 1) x 4, etc.
-# ^ This will be the size of the matrices
-
-
-from random import randrange
-from functools import partial
-
-def random(a, b):
-    return randrange(a, b)
-
-my_func = partial(random, -5, 5)
-
-
-
-def calc_deltas(curr_deltas, prev_weights):
-    prev_deltas = []
-    # more list comps here? Or break it out?
-    for i in xrange(1, len(prev_weights[0])):    # could do with self.sizes?
-        total = sum( curr_deltas[j][0] * weights[i]
-                     for j, weights in enumerate(prev_weights) )
-        prev_deltas.append([total])
-
-    return prev_deltas
-
-
-curr_deltas = [[2], [1]]
-prev_weights = [[1, 2, 3, 4], [4, 3, 2, 1]]
-
-print calc_deltas(curr_deltas, prev_weights)
